3e8ec3c833bde725add676280d0db343149a3290
刘帅帅 <7776195+liu_shuai_shuai_lss@user.noreply.gitee.com>
2026-01-08 16:35:38 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ const loadDishes = async () => {
 
 const loadOrderHistory = async () => {
   try {
-    const response = await fetch("https://154283868.github.io/caipu/orders");
+    const response = await fetch("./orders.json");
     orderHistory.value = await response.json();
   } catch (error) {
     console.error("加载历史订单失败:", error);
